1e990d559574af837111e9bad9b8cccd139994fa
Wong bang <Bangwong474@gmail.com>
2026-02-23 05:43:32 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+require('dotenv').config();
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const PORT=process.env.PORT||3000;
+
+let user=[];
+
+app.post('/signup',(req,res)=>{const{email,password}=req.body;if(!email||!password)
+return res.status(400).send('Fill all fields');const exist=users.find(u=> u.email===email);
+if(exist) return res.status(400).send('User exists');users.push({email,password});
+res.send('sign up successfully');});
+
+
+app.post('/login',(req,res)=>{const{email,password}=req.body;
+const user=users.find(u=>u.email===email & u.password===password);
+if(!user)return res.status(401).send('Invalid credentials');
+res.send('Login successfully');});
+
+app.listen(PORT,()=>console.log(`Server running on http://localhost:${PORT}`));
+
+app.get('/test',(req,res)=>{res.send('Your server work successfully');});
