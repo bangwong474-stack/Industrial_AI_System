55af2019b4247ff82a60199597dd8a65c3397c05
Wong bang <Bangwong474@gmail.com>
2026-02-25 15:08:11 -0800

    fix backend module errors

--- backend/ai_machine.py
+++ backend/ai_machine.py
@@ -1,13 +1,13 @@
-import pickle
+#import pickle
 
 with open("backend/production.pkl","rb")as file:
-    model=pickle.load(file)
+    #model=pickle.load(file)
 
-def predict(features):
-    result=model.predict([features])
-    return result
+#def predict(features):
+   # result=model.predict([features])
+    # return result
 
-def machine_failure_rate(features):
-    prediction=model.predict([features])
-    machine_failure_rate=max(0,1-prediction[0]/100)
-    return machine_failure_rate
+#def machine_failure_rate(features):
+   # prediction=model.predict([features])
+   # machine_failure_rate=max(0,1-prediction[0]/100)
+    #return machine_failure_rate
