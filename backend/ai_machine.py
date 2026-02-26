7f2d23bbd7975a94a952f6766507c8f14a94b6d1
Wong bang <Bangwong474@gmail.com>
2026-02-26 01:01:42 -0800

    fix backend model errors

--- backend/ai_machine.py
+++ backend/ai_machine.py
@@ -1,12 +1,12 @@
 import joblib
 
-#model=joblib.load("production.pkl")
+model=joblib.load("production.pkl")
 
-#def predict(input_data):
-   # prediction=model.predict([input data])
-    # return prediction.tolist()
+def predict(input_data):
+   prediction=model.predict([input data])
+   return prediction.tolist()
 
-#def machine_failure_rate(features):
-   # prediction=model.predict([features])
-   # machine_failure_rate=max(0,1-prediction[0]/100)
-    #return machine_failure_rate
+def machine_failure_rate(features):
+    prediction=model.predict([features])
+    machine_failure_rate=max(0,1-prediction[0]/100)
+    return machine_failure_rate
