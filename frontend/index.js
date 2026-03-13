cbf4bebb9379a4574f488456e04fdb3f2444a2f4
Wong bang <Bangwong474@gmail.com>
2026-03-13 10:26:30 -0700

    fix frontend js

--- frontend/index.js
+++ frontend/index.js
@@ -1,26 +1,23 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-require('dotenv').config();
+async function sendMessage(){
 
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+const message=document.getElementById("message").Value;
 
-const PORT=process.env.PORT||3000;
+const response=await
+fetch("https://Industrial_AI_System.onrender.com/chat",{
 
-let user=[];
+method:"POST",
 
-app.post('/signup',(req,res)=>{const{email,password}=req.body;if(!email||!password)
-return res.status(400).send('Fill all fields');const exist=users.find(u=> u.email===email);
-if(exist) return res.status(400).send('User exists');users.push({email,password});
-res.send('sign up successfully');});
+headers:{"Content-Type":"application/json"
+},
 
+body:JSON.stringify({
+message:message
+})
 
-app.post('/login',(req,res)=>{const{email,password}=req.body;
-const user=users.find(u=>u.email===email & u.password===password);
-if(!user)return res.status(401).send('Invalid credentials');
-res.send('Login successfully');});
+});
 
-app.listen(PORT,()=>console.log(`Server running on http://localhost:${PORT}`));
+const data=await response.json();
 
-app.get("/",(req,res)=>{res.send("Industrial AI system is live")});
+document.getElementById("reply").innerText=data.reply;
+
+}
