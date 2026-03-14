b5370d28aa515f7ef313d1a0a37b8964ef5c09b6
Wong bang <Bangwong474@gmail.com>
2026-03-14 07:02:44 -0700

    added login frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,10 @@
-async function sendMessage(){
+async function login(){
 
-const message=document.getElementById("message").value;
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
 
 const response=await
-fetch("https://industrial-ai-system.onrender.com/chat",{
+fetch("https://industrial-ai-system.onrender.com/login",{
 
 method:"POST",
 
@@ -11,13 +12,14 @@ headers:{"Content-Type":"application/json"
 },
 
 body:JSON.stringify({
-message:message
+email:email,
+password:password
 })
 
 });
 
 const data=await response.json();
 
-document.getElementById("reply").innerText=data.reply;
+document.getElementById("result").innerText=data.message;
 
 }
