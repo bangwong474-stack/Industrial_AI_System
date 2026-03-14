a2556a85eea640d1e62ac48173a684fdaacd2353
Wong bang <Bangwong474@gmail.com>
2026-03-14 11:41:24 -0700

    added is_active for subscription system

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String,Float,ForeignKey
+from sqlalchemy import Column, Integer, String,Float,ForeignKey,Boolean
 from database import Base
 
 class Factory(Base):
@@ -10,9 +10,10 @@ class Factory(Base):
 class User(Base):
     __tablename__="users"
 
-    id=Column(Integer,primary_key=True)
-    email=Column(String)
+    id=Column(Integer,primary_key=True,index=True)
+    email=Column(String,unique=True,index=True)
     password=Column(String)
+    is_active=Column(Boolean,default=False)
     subscription=Column(String)
 
 class ProductionData(Base):
