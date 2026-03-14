92ea1867bef9fdf44a57891905e4d7c43b320aeb
Wong bang <Bangwong474@gmail.com>
2026-03-14 06:11:33 -0700

    fixed database connection

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+import os
+
+DATABASE_URL=os.getenv("DATABASE_URL")
 
 engine= create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
